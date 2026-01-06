dffd14d1a30e3e933789ad69785f5b0a9417b962
Luiz Lira <lbrandaolira@gmail.com>
2026-01-05 22:26:50 -0400

    trying to connect both ms with rabbit

--- user-service/src/main/java/luizbrand/com/user_service/infrastructure/producer/UserProducer.java
+++ user-service/src/main/java/luizbrand/com/user_service/infrastructure/producer/UserProducer.java
@@ -1,7 +1,7 @@
 package luizbrand.com.user_service.infrastructure.producer;
 
 import luizbrand.com.user_service.infrastructure.persistence.UserEntity;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Component;
 public class UserProducer {
 
     private final RabbitTemplate rabbitTemplate;
+    private final FanoutExchange fanout;
 
-    public UserProducer(RabbitTemplate rabbitTemplate) {
+    public UserProducer(RabbitTemplate rabbitTemplate, FanoutExchange fanout) {
         this.rabbitTemplate = rabbitTemplate;
+        this.fanout = fanout;
     }
 
     public void sendUserCreatedEvent(UserEntity userEntity) {
