2028ab6cd8379237a81115f8807d900aa2abae38
Luiz Lira <lbrandaolira@gmail.com>
2026-01-06 23:36:55 -0400

    refactor(email-service): update email consumer logic

--- email-service/src/main/java/luizbrand/com/email_service/infrastructure/consumer/EmailConsumer.java
+++ email-service/src/main/java/luizbrand/com/email_service/infrastructure/consumer/EmailConsumer.java
@@ -1,8 +1,10 @@
 package luizbrand.com.email_service.infrastructure.consumer;
 
 import luizbrand.com.email_service.infrastructure.dto.EmailDTO;
+import luizbrand.com.email_service.infrastructure.persistence.EmailEntity;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +13,9 @@ public class EmailConsumer {
 
     //Listener do consumer
     @RabbitListener(queues = "email-queue")
-    @RabbitHandler
     public void listenEmailQueue(@Payload EmailDTO emailDTO) {
-        System.out.println(emailDTO.emailTo());
+        EmailEntity email = new EmailEntity();
+        BeanUtils.copyProperties(emailDTO, email);
     }
 
 }
